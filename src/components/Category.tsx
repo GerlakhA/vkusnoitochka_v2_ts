1ec54955adc8aa76e6dc91822da05d8b99a83893
Andrey Gerlakh <andrey@MacBook-Air-Andrey.local>
2023-10-08 15:46:36 +0300

    test adaptive category in header

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Category = () => {
 	return (
-		<div className='mr-[90px] ml-20 xl: flex-col'>
+		<div className='mr-[90px] ml-20 md:flex-col'>
 			<Link className='m-5 hover:opacity-60 focus:font-extrabold' to={'/menu'}>
 				Меню
 			</Link>
